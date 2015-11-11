e4fe9164343f934e4af694e88ba83db266493491
Chris Atchley <steve.chris.atchley@gmail.com>
2015-11-10 21:19:25 -0500

    Finished "Self Destruction".  Finished Build a self-destructing message app on treehouse.

--- app/src/main/java/atchley/chris/ribbot/InboxFragment.java
+++ app/src/main/java/atchley/chris/ribbot/InboxFragment.java
@@ -17,6 +17,7 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -55,8 +56,13 @@ public class InboxFragment extends ListFragment {
                     }
 
 
-                    MessageAdapter adapter  = new MessageAdapter(getListView().getContext(), mMessages);
-                    setListAdapter(adapter);
+                    if (getListView().getAdapter() == null) {
+                        MessageAdapter adapter = new MessageAdapter(getListView().getContext(), mMessages);
+                        setListAdapter(adapter);
+                    } else {
+                        //refill adapter
+                        ((MessageAdapter) getListView().getAdapter()).refill(mMessages);
+                    }
                 }
             }
         });
@@ -71,18 +77,34 @@ public class InboxFragment extends ListFragment {
         ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
         Uri fileUri = Uri.parse(file.getUrl());
 
-        if (messageType.equals(ParseConstants.TYPE_IMAGE)){
+        if (messageType.equals(ParseConstants.TYPE_IMAGE)) {
             //view image
             Intent intent = new Intent(getActivity(), ViewImageActivity.class);
             intent.setData(fileUri);
             startActivity(intent);
-        }
-        else {
+        } else {
             //view video
             Intent intent = new Intent(Intent.ACTION_VIEW, fileUri);
-            intent.setDataAndType(fileUri,"video/*");
+            intent.setDataAndType(fileUri, "video/*");
             startActivity(intent);
         }
+
+        //delete message
+        List<String> ids = message.getList(ParseConstants.KEY_RECIPIENT_IDS);
+
+        if (ids.size() == 1) {
+            //last recipient-delete whole thing
+            message.deleteInBackground();
+        } else {
+            //remove recipient and save
+            ids.remove(ParseUser.getCurrentUser().getObjectId());
+            ArrayList<String> idsToRemove = new ArrayList<String>();
+            idsToRemove.add(ParseUser.getCurrentUser().getObjectId());
+
+            message.removeAll(ParseConstants.KEY_RECIPIENT_IDS, idsToRemove);
+            message.saveInBackground();
+        }
+
     }
 }
 
--- app/src/main/java/atchley/chris/ribbot/MainActivity.java
+++ app/src/main/java/atchley/chris/ribbot/MainActivity.java
@@ -322,16 +322,19 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
             case R.id.action_logout:
                 ParseUser.logOut();
                 navigateToLogin();
+                break;
 
             case R.id.action_edit_friends:
                 Intent intent = new Intent(this, EditFriendsActivity.class);
                 startActivity(intent);
+                break;
 
             case R.id.action_camera:
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setItems(R.array.camera_choices, mDialogListener);
                 AlertDialog dialog = builder.create();
                 dialog.show();
+                break;
         }
 
 
